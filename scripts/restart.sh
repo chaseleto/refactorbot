1dc87ea482a607f1e72670ab5dd8839c13941ea7
Chase Letourneau <cal20518@gmail.com>
2022-12-30 18:43:51 -0500

    test

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,7 +1,7 @@
 sudo rm -r /opt/docker/discordbot_data/src && \
-export DISCORD_SECRET=$(gcloud secrets versions access latest --secret="DISCORD_SECRET") && \
 cd /refactorbot/ && \
 sudo git pull  && \
 sudo cp -Rp src /opt/docker/discordbot_data/src/ && \
 sudo ./scripts/volumes.sh && \
+export DISCORD_SECRET=$(gcloud secrets versions access latest --secret="DISCORD_SECRET") && \
 sudo docker compose -f smoot-compose.yml up -d
