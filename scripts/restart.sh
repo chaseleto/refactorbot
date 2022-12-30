d57aa28830bb62504512585d3d445f0ebcffd547
Chase Letourneau <cal20518@gmail.com>
2022-12-30 18:17:05 -0500

    test

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,7 +1,5 @@
-sudo rm -r /opt/docker/discordbot_data/src
-export MONGO_USER=$(gcloud secrets versions access latest --secret="MONGO_USER") && \
-export MONGO_PASSWORD=$(gcloud secrets versions access latest --secret="MONGO_PASSWORD") && \
-export DISCORD_SECRET=$(gcloud secrets versions access latest --secret="DISCORD_SECRET") && \
+sudo rm -r /opt/docker/discordbot_data/src && \
+sudo export DISCORD_SECRET=$(gcloud secrets versions access latest --secret="DISCORD_SECRET") && \
 cd /refactorbot/ && \
 sudo git pull  && \
 sudo cp -Rp src /opt/docker/discordbot_data/src/ && \
