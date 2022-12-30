460f22c8537a46a63c701c80e21b23b58bc532e0
Chase Letourneau <cal20518@gmail.com>
2022-12-30 18:53:49 -0500

    fix

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,4 +4,4 @@ sudo git pull  && \
 sudo cp -Rp src /opt/docker/discordbot_data/src/ && \
 sudo ./scripts/volumes.sh && \
 export DISCORD_SECRET=$(gcloud secrets versions access latest --secret="DISCORD_SECRET") && \
-sudo docker compose -f smoot-compose.yml up -d
+docker compose -f smoot-compose.yml up -d
