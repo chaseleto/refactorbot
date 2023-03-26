92ba5991788f7d391181922942490849623cba27
chaseleto <74798031+chaseleto@users.noreply.github.com>
2023-03-26 13:05:48 -0400

    fix script

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -6,4 +6,5 @@ sudo ./scripts/volumes.sh && \
 export MONGO_USER=$(gcloud secrets versions access latest --secret="MONGO_USER") && \
 export MONGO_PASSWORD=$(gcloud secrets versions access latest --secret="MONGO_PASSWORD") && \
 export DISCORD_SECRET=$(gcloud secrets versions access latest --secret="DISCORD_SECRET") && \
+export LOLAPI=$(gcloud secrets versions access latest --secret="LOLAPI") && \
 docker compose -f smoot-compose.yml up --force-recreate -d
