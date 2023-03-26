6cba50f599cd795d7207e127a0449c0b9c3e4f9e
chaseleto <74798031+chaseleto@users.noreply.github.com>
2023-03-26 17:05:18 -0400

    fix

--- src/cogs/tracking.py
+++ src/cogs/tracking.py
@@ -71,8 +71,8 @@ class tracker(commands.Cog):
         most_played_champ = ""
         champ_count = {}
         embed = discord.Embed(title=f"{message} | {rank}", color=color, description=ingame, url=f"https://na.op.gg/summoner/userName={message}")
-        for idx, match in enumerate(my_matches[:10]):
-            match_detail = lol_watcher.match.by_id(my_region, match)
+        for idx, match1 in enumerate(my_matches[:10]):
+            match_detail = lol_watcher.match.by_id(my_region, match1)
             #print(match_detail)
             queueType = ''
             queueId = match_detail['info']['queueId']
